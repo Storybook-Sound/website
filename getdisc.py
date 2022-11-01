662e05eab09f290549976a2d11f2d8f514bd8be5
mikeill <mike@mzoo.org>
2022-11-01 11:15:23 -0500

    Get images by component (src, title, etc).

--- getdisc.py
+++ getdisc.py
@@ -3,6 +3,10 @@ import urllib.request
 import phpserialize
 
 with open("disc.xml") as f: soup = BeautifulSoup(f, "xml")
+with open("media.xml") as f: media = BeautifulSoup(f, "xml")
+
+def quote_str(str):
+  return "'" + str.replace("'", "''") + "'"
 
 with open("_data/discography.yml", "w") as disco:
   for item in soup.find_all("item"):
@@ -12,7 +16,7 @@ with open("_data/discography.yml", "w") as disco:
         if elem.name == "meta_value":
           metadata[meta.text] = elem.text.strip()
           break
-    print('- project: ' + item.title.text.replace(":", "&#58;"), file=disco)
+    print('- project: ' + quote_str(item.title.text), file=disco)
     if (set := item.find('category', {"domain": "artist"})):
       print('  artist: ' + item.find('category', {"domain": "artist"}).text, file=disco)
     if (set := item.find('category', {"domain": "set"})):
@@ -28,11 +32,16 @@ with open("_data/discography.yml", "w") as disco:
       for k,v in urlparts.items():
         print('    ' + k.decode() + ': ' + v.decode(), file=disco )
     if notes := metadata.get('additional_notes'):
-      print('  notes: ' + notes, file=disco)
+      print('  notes: >-', file=disco)
+      print('    ' + notes.replace("\n", "\n    "), file=disco)
     if (set := item.find('guid', {"isPermaLink": "false"})):
       try:
-        image_url = item.find('guid', {"isPermaLink": "false"}).text
-        print('  image: ' + image_url.replace("wp/wp-content", "app"), file=disco)
+
+        if thumb := metadata.get('_thumbnail_id'):
+          attachment = media.find('post_id', text=thumb).parent
+          print('  image: ', file=disco )
+          print('    url: ' + attachment.attachment_url.text, file=disco )
+          print("    title: " + quote_str(attachment.title.text), file=disco )
         # print('  image_filename: ' + image_url.split('/')[-1], file=disco)
         # urllib.request.urlretrieve(image_url, "disco_images/" + item.find('guid', {"isPermaLink": "false"}).text.split('/')[-1])
       except urllib.error.HTTPError:
