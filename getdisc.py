59dd41da4e3b5c88163fb63dc528200c4196891b
mikeill <mike@mzoo.org>
2022-11-02 09:34:27 -0500

    Add images and also unescape some HTML chars.

--- getdisc.py
+++ getdisc.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import urllib.request
 import phpserialize
+import os
+import html
 
 with open("disc.xml") as f: soup = BeautifulSoup(f, "xml")
 with open("media.xml") as f: media = BeautifulSoup(f, "xml")
 
 def quote_str(str):
-  return "'" + str.replace("'", "''") + "'"
+  return "'" + html.unescape(str).replace("'", "''") + "'"
 
 class YearDict(dict):
   """
@@ -32,7 +35,7 @@ for item in soup.find_all("item"):
   year = set.text
   disco = years[year]
   print('- project: ' + quote_str(item.title.text), file=disco)
-  print('  artist: ' + item.find('category', {"domain": "artist"}).text, file=disco)
+  print('  artist: ' + quote_str(item.find('category', {"domain": "artist"}).text), file=disco)
   print('  year: ' + year, file=disco)
   print('  roles:', file=disco)
   for role in item.find_all('category', {"domain": "project_role"}):
@@ -48,14 +51,15 @@ for item in soup.find_all("item"):
     print('  notes: >-', file=disco)
     print('    ' + notes.replace("\n", "\n    "), file=disco)
   if (set := item.find('guid', {"isPermaLink": "false"})):
-    try:
-
-      if thumb := metadata.get('_thumbnail_id'):
-        attachment = media.find('post_id', text=thumb).parent
-        print('  image: ', file=disco )
-        print('    url: ' + attachment.attachment_url.text, file=disco )
-        print("    title: " + quote_str(attachment.title.text), file=disco )
-      # print('  image_filename: ' + image_url.split('/')[-1], file=disco)
-      # urllib.request.urlretrieve(image_url, "disco_images/" + item.find('guid', {"isPermaLink": "false"}).text.split('/')[-1])
-    except urllib.error.HTTPError:
-      pass
+    if thumb := metadata.get('_thumbnail_id'):
+      os.makedirs("images/discography/" + year, exist_ok=True)
+      attachment = media.find('post_id', text=thumb).parent
+      filename = "images/discography/" + year + "/" + os.path.basename(attachment.attachment_url.text)
+      print('  image: ', file=disco )
+      print('    url: ' + filename, file=disco )
+      print("    title: " + quote_str(attachment.title.text), file=disco )
+      if not os.path.exists(filename):
+        try:
+          urllib.request.urlretrieve(attachment.attachment_url.text, filename)
+        except urllib.error.HTTPError:
+          print("Couldn't fetch ", filename)
