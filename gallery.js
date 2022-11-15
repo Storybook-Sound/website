799ba746d9861b58efd0b6a1b4a7514c32e9a7a4
mikeill <mike@mzoo.org>
2022-11-15 12:02:49 -0600

    Separate projects without image and display as list.

--- gallery.js
+++ gallery.js
@@ -1,5 +1,5 @@
 import {choc, set_content, on, DOM, fix_dialogs} from "https://rosuav.github.io/choc/factory.js";
-const {DIV, IMG, H1, H2, H3, H4, A, DIALOG, FIGURE, FIGCAPTION, BUTTON, P, SECTION, UL, LI, SPAN} = choc; //autoimport
+const {DIV, IMG, H1, H2, H3, H4, A, DIALOG, FIGURE, FIGCAPTION, BUTTON, P, SECTION, UL, LI, SPAN, BLOCKQUOTE} = choc; //autoimport
 
 let selected_item = 0;
 let selected_set = 0;
@@ -16,8 +16,6 @@ if (window.client_listing === true) {
   }));
   galleries = artists;
 }
-
-const placeholder_colors = ['red', 'gold', 'green', 'blue', 'teal', 'azure'];
 
 const sets = Object.keys(galleries);
 if (window.client_listing === true) {
@@ -40,9 +38,7 @@ document.body.appendChild(DIALOG({id: "gallerydlg"}, [
 fix_dialogs({close_selector: ".dialog_cancel,.dialog_close", click_outside: "formless"});
 
 function gallery_image(item) {
-  if (item.image) return DIV({class: 'gallery-image', style: `background-image: url("${item.image.url}")`, title: item.project + ' ' + item.artist});
-  let color = placeholder_colors[Math.floor(Math.random() * 5)];
-  return DIV({class: 'placeholder gallery-image ' + color}, DIV([H1(item.project),H2(item.artist)]));
+  return DIV({class: 'gallery-image', style: `background-image: url("${item.image.url}")`, title: item.project + ' ' + item.artist});
 }
 
 
@@ -51,7 +47,11 @@ if (window.client_listing === true) {
     sets.map((set, idx) => LI({'data-set': idx},
       [set, SPAN([
         " (",
-          galleries[set].map((item, idx) => [idx && ", ", A({href: "#", "data-idx": idx, class: "gallery_entry"}, item.project || item.year)]),
+        galleries[set].map((item, idx) => [
+          idx &&
+          ", ", item.image ?
+            A({href: "#", "data-idx": idx, class: "gallery_entry"}, item.project || item.year)
+            : item.project || item.year]),
         ")"])]
         )
       )
@@ -62,7 +62,14 @@ if (window.client_listing === true) {
   set_content("#gallery",
     sets.map((set, idx )=> SECTION({'data-set': idx}, [
       H2(set),
-      DIV({class: "gallery_set"}, galleries[set].map((item, idx) => DIV({"data-idx": idx, class: "gallery_entry"}, gallery_image(item) )))
+      DIV({class: "gallery_set"}, galleries[set].map((item, idx) => item.image && DIV({"data-idx": idx, class: "gallery_entry"}, gallery_image(item)))),
+      P(UL(galleries[set].map((item, idx) => !item.image &&
+        LI([
+          item.artist, ", ", item.project,
+          item.roles && " (" + item.roles.join(", ") + ") ",
+          item.project_url && A({href: item.project_url['url'], target: "_blank"}, item.project_url['title']),
+          item.notes && BLOCKQUOTE(item.notes),
+        ]))))
     ]))
   );
 }
@@ -85,23 +92,34 @@ function display_item(set, idx) {
 }
 
 on("click", ".gallery_entry", e => {
+  e.preventDefault();
   display_item(e.match.closest("[data-set]").dataset.set, e.match.closest("[data-idx]").dataset.idx);
   DOM("#gallerydlg").showModal();
 });
 
 on("click", "#prev", e => {
-  if (selected_item) {
-    display_item(selected_set, selected_item - 1);
-  } else {
-    const set = selected_set ? selected_set - 1 : sets.length - 1;
-    display_item(set, galleries[sets[set]].length - 1);
-  }
+  let set = selected_set, item = selected_item;
+  do {
+    if (item) --item; else {
+      set = set ? set - 1 : sets.length - 1;
+      item = galleries[sets[set]].length - 1;
+    }
+    if (galleries[sets[set]][item].image) break;
+  } while (item != selected_item || set !== selected_set);
+
+  display_item(set, item);
+
 });
 
 on("click", "#next", e => {
-  if (selected_item < galleries[sets[selected_set]].length - 1) {
-    display_item(selected_set, selected_item + 1);
-  } else {
-    display_item((selected_set + 1) % sets.length, 0);
-  }
+  let set = selected_set, item = selected_item;
+  do {
+    if (item < galleries[sets[set]].length - 1) ++item; else {
+      set = (set + 1) % sets.length;
+      item = 0;
+    }
+    if (galleries[sets[set]][item].image) break;
+  } while (item != selected_item || set !== selected_set);
+
+  display_item(set, item);
 });
