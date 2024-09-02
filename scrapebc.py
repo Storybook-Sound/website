7961382b4c19b0fc11f7b878a0a25af7b74e5904
mikeill <mike@mzoo.org>
2024-09-02 17:39:22 -0500

    Get album title, artist and image.

--- scrapebc.py
+++ scrapebc.py
@@ -4,16 +4,33 @@ from bs4 import BeautifulSoup
 
 from urllib.parse import urljoin
 
-print("Enter your URL:")
-url = input()
 
-r = requests.get(url)
+urls = [
+    "https://meganreilly.bandcamp.com/album/megan-reilly",
+    "https://chrisharford.bandcamp.com/album/looking-out-for-number-6?label=2914117867&tab=music"
+]
 
-soup = BeautifulSoup(r.content, "html.parser")
+for url in urls:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
 
-album_links = []
-for album in soup.find_all('a', href=re.compile(r"\/album/.+")):
-    album_url = album.get('href')
-    album_links.append(urljoin(url, album_url))
+    namesection = soup.findAll('div', {'id':'name-section'})[0]
+    print(namesection)
 
-print(len(album_links), album_links)
+    albumart = soup.findAll('div', {'id':'tralbumArt'})[0].find('img').get('src')
+    print(albumart)
+
+    artist_with_by = soup.findAll('h3')[0].get_text()
+    print("Artist with 'by': ", artist_with_by)
+
+    artist = soup.findAll('h3')[0].findAll('span')[0].findAll('a')[0].get_text()
+    print("Artist: ", artist)
+
+    trackTitle = soup.findAll('h2', {'class': 'trackTitle'})[0].get_text()
+
+    """ album_links = []
+    for album in soup.find_all('a', href=re.compile(r"\/album/.+")):
+        album_url = album.get('href')
+        album_links.append(urljoin(url, album_url))
+
+    print(len(album_links), album_links) """
