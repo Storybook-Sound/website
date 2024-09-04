2ddf768184dcaac56663f0c9ac93c04469491b21
mikeill <mike@mzoo.org>
2024-09-04 15:39:40 -0500

    Generate full file from dummy data.

--- scrapebc.py
+++ scrapebc.py
@@ -11,7 +11,7 @@ urls = [
 ]
 
 with open('latest.csv', newline='') as l:
-  reader = csv.DictReader(l)
+  reader = csv.DictReader(l, delimiter='	')
   for row in reader:
     print(row["url"], row["year"], row["roles"], row["notes"])
 
@@ -19,25 +19,33 @@ with open('latest.csv', newline='') as l:
     soup = BeautifulSoup(r.content, "html.parser")
 
     namesection = soup.findAll('div', {'id':'name-section'})[0]
-    print(namesection)
+    #print(namesection)
 
     albumart = soup.findAll('div', {'id':'tralbumArt'})[0].find('img').get('src')
-    print(albumart)
+    #print(albumart)
 
     artist_with_by = soup.findAll('h3')[0].get_text()
-    print("Artist with 'by': ", artist_with_by)
+    #print("Artist with 'by': ", artist_with_by)
 
     artist = soup.findAll('h3')[0].findAll('span')[0].findAll('a')[0].get_text()
-    print("Artist: ", artist)
+    #print("Artist: ", artist)
 
-    trackTitle = soup.findAll('h2', {'class': 'trackTitle'})[0].get_text()
-    with open('_data/discography/%s.yml' % row["year"].strip(), 'a+') as f:
-      f.write("\n- project: '%s'\n" % trackTitle.strip())
-      f.write("\t  artist: '%s'" % artist.strip())
-
-    """ album_links = []
-    for album in soup.find_all('a', href=re.compile(r"\/album/.+")):
-        album_url = album.get('href')
-        album_links.append(urljoin(url, album_url))
-
-    print(len(album_links), album_links) """
+    trackTitle = soup.findAll('h2', {'class': 'trackTitle'})[0].get_text().strip()
+    print("Track Title: ", trackTitle)
+    year = row["year"].strip()
+    roles = [r.strip() for r in row["roles"].split(',')]
+    with open('_data/discography/%s.yml' % year, 'a+') as f:
+      f.write("- project: '%s'\n" % trackTitle.strip())
+      f.write("  artist: '%s'\n" % artist.strip())
+      f.write("  year: %s\n" % year)
+      f.write("  roles:\n")
+      for role in roles:
+        f.write("    - %s\n" % role)
+      f.write("  project_url:\n")
+      f.write("    url: '%s'\n" % row["url"].strip())
+      f.write("    title: '%s'\n" % "Artist Site")
+      f.write("  notes: >-\n")
+      f.write("    <b>%s</b>\n\n" % row["notes"].strip())
+      f.write("  image:\n")
+      f.write("    url: '%s'\n" % albumart)
+      f.write("    title: '%s %s'\n\n" % (artist.strip(), '"'+trackTitle.strip()+'"'))
