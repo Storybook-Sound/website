c255227d4b17404d4fa15f6fa3b081f782c39288
mikeill <mike@mzoo.org>
2025-01-11 20:42:10 -0600

    Some ansi color output.

--- checkpresence.py
+++ checkpresence.py
@@ -1,29 +1,46 @@
 import re
+import os
+
+def findfiles(path, regex):
+    regObj = re.compile(regex)
+    res = []
+    for root, dirs, fnames in os.walk(path):
+        for fname in fnames:
+            if regObj.match(fname):
+                res.append(os.path.join(root, fname))
+    return res
+
+
 years = ["2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"]
 for year in years:
   print(f"Checking for {year}...")
   try:
     with(open(f"test/{year}.yml", "r")) as f:
       lines_with_project = [line for line in f.readlines() if re.search(r"project", line)]
-      print(f"Fetched {len(lines_with_project)} projects for {year}...")
+      #print(f"Fetched {len(lines_with_project)} projects for {year}...")
       for line in lines_with_project:
         try:
           project = re.search(r"- project: '(.*)'", line).group(1)
         except AttributeError:
           continue
-        print(f"Checking for {project} in {year}...")
-        try:
-          with(open(f"_data/discography/{year}.yml", "r")) as f:
-            matched_lines = [line for line in f.readlines() if re.search(r"project", line) and re.search(project, line)]
-            print(f"Matched {len(matched_lines)} lines for {project} in {year}...")
-            if len(matched_lines) == 0:
-              print(f"Project {project} not found in {year}...")
-            else:
-              for matched_line in matched_lines:
-                print("Matched Line: %s" % matched_line)
-        except FileNotFoundError:
-          print(f"Current File not found for {year}...")
-          continue
+        #print(f"Checking for {project} in {year}...")
+        currentfiles = next(os.walk("_data/discography/"), (None, None, []))[2]  # [] if no file
+        found = False
+        for fname in currentfiles:
+          if os.path.isfile(f"_data/discography/{fname}"):
+              # Full path
+              f = open(f"_data/discography/{fname}", 'r')
+
+              if project.lower() in f.read().lower():
+                  print('\t \033[46;1m(found %s in file %s)\033[0m' % (project, fname))
+                  found = True
+                  break
+              else:
+                  pass
+                  #print('Project %s not found in file %s' % (project, fname))
+              f.close()
+        if not found:
+          print(f"\033[31;1m!!!DID NOT FIND: Project {project} \033[0m \n")
   except FileNotFoundError:
     print(f"No Read File found for {year}...")
     continue
