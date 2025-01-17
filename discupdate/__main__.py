e72f1988464be20e21b882328e14264cbecd224e
mikeill <mike@mzoo.org>
2025-01-17 15:43:34 -0600

    Refine to generate import file, check for presence.

--- discupdate/__main__.py
+++ discupdate/__main__.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import csv
 from urllib.parse import urljoin, quote_plus
 from . import configs
+from datetime import datetime
 # https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
 # https://console.cloud.google.com/apis/api/customsearch.googleapis.com/metrics?inv=1&invt=AbnGmw&project=search-api-story-1737131699378
 # https://stackoverflow.com/a/37084643/2223106
@@ -25,11 +26,32 @@ for start in range(1, 100, 10):
 
 rolesmapping = {}
 
+todaysdateandtime = datetime.now().strftime("%Y-%m-%d-%H-%M")
+
 for link in links:
     r = requests.get(link)
     soup = BeautifulSoup(r.content, "html.parser")
     albumCredits = soup.findAll('div', {'class': 'tralbumData tralbum-credits'})[0].get_text()
-    roles = [r.strip() for r in albumCredits.split('\n') if re.search(r"Scott Anthony|Storybook Sound", r, re.IGNORECASE)] if re.search(r"Scott Anthony|Storybook Sound", albumCredits, re.IGNORECASE) else ["Undefined"]
+    roles = []
+    if re.search(r"Scott Anthony|Storybook Sound", albumCredits, re.IGNORECASE):
+      roles = [r.strip() for r in albumCredits.split('\n')]
+    else: roles = ["Undefined"]
+    year = re.search(r"(\d{4})", albumCredits).group(1) if re.search(r"(\d{4})", albumCredits) else "UNKNOWN"
+    # Check for presence in discography
+    try:
+      with(open(f"_data/discography/{year}.yml", "r")) as f:
+        lines_with_project = [line for line in f.readlines() if re.search(r"project", line)]
+        for line in lines_with_project:
+          try:
+            project = re.search(r"- project: '(.*)'", line).group(1)
+          except AttributeError:
+            continue
+          if project.lower().replace("'", "&#39;") in albumCredits.lower():
+            print(f"\033[46;1m(found {project} in {link})\033[0m\n")
+            break
+    except FileNotFoundError:
+      print(f"No Read File found for {year}...")
+      continue
     if roles == ["Undefined"]:
         print(f"\033[31;1m!!!DID NOT FIND: Scott Anthony or Storybook Sound in {link} \033[0m \n")
         # make this one yellow
@@ -38,10 +60,10 @@ for link in links:
     else:
         print(f"\033[46;1m(found Scott Anthony in {link})\033[0m\n")
         rolesmapping[link] = roles
-with open('rolescheck.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(["url", "roles"])
-    for key, value in rolesmapping.items():
-        writer.writerow([key, value])
+with open(f'discimports/{todaysdateandtime}.csv', 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=['url', 'year', 'roles', 'notes'])
+    writer.writeheader()
+    for link, roles in rolesmapping.items():
+        writer.writerow({'url': link, 'year': year, 'roles': ', '.join(roles), 'notes': ''})
 
-print(f"Found {len(links)} links. Done.")
+print(f"Wrote import file for {len(links)} links.")
