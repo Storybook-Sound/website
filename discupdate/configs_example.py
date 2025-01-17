6f4b0ac8a2e6988a39bc78337f9d4639528a0fd4
mikeill <mike@mzoo.org>
2025-01-17 14:41:48 -0600

    Update link to google api screen.

--- discupdate/configs_example.py
+++ discupdate/configs_example.py
@@ -1,2 +1,2 @@
-googleapikey = 'get from https://console.cloud.google.com/apis/api'
+googleapikey = 'get from https://console.cloud.google.com/apis/credentials'
 custom_search_engine_id = 'get from http://www.google.com/cse/'
